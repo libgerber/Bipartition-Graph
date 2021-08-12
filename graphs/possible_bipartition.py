9d07dd24bdc5bc6a150614e790cb03791e998b7f
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-12 08:42:33 -0700

    rename lib folder

---
+++ graphs/possible_bipartition.py
@@ -0,0 +1,9 @@
+
+def possible_bipartition(dislikes):
+    """ Will return True or False if the given graph
+        can be bipartitioned without neighboring nodes put
+        into the same partition.
+        Time Complexity: ?
+        Space Complexity: ?
+    """
+    pass
