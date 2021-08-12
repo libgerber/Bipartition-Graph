4b87832574e74ee8ce126a93496aeae507847973
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-12 10:54:09 -0700

    initial starter code

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,23 +1,5 @@
-from collections import deque
-
-COLORS = ["red", "green"]
-
-def dfs(dislikes, current_node, painted_graph, current_color):
-    neighbors = dislikes[current_node]
-    next_color = (current_color + 1) % len(COLORS)
-
-    for neighbor in neighbors:
-        color = painted_graph.get(neighbor)
-
-        if not color:
-            painted_graph[neighbor] = COLORS[next_color]
-            if not dfs(dislikes=dislikes, current_node=neighbor, painted_graph=painted_graph, current_color=next_color):
-                return False
-        elif color != COLORS[next_color]:
-            return False
-    
-    return True
-    
+# Can be used for BFS
+from collections import deque 
 
 def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
@@ -26,14 +8,5 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    painted_graph = {}
-    current_color = 0
+    pass
 
-    for node in range(len(dislikes)):
-        neighbors = dislikes[node]
-        if not painted_graph.get(node):
-            painted_graph[node] = COLORS[current_color]
-
-            if not dfs(dislikes=dislikes, current_node=node, painted_graph=painted_graph, current_color=current_color):
-                return False
-    return True
